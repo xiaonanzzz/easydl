25bab939d23ce37b85ef51621341fd2080760e3e
dfpipe <dfpipe2024@gmail.com>
2025-05-20 20:28:38 -0700

    add pre-trained imagenet wrapper

---
+++ tests/real_test/test_resent_clf.py
@@ -0,0 +1,11 @@
+
+from easydl.clf.pytorch_models import create_imagenet_resnet18_classifier
+
+def test_imagenet_resnet18_classifier():
+    classifier = create_imagenet_resnet18_classifier()
+    image = 'https://images.pexels.com/photos/406014/pexels-photo-406014.jpeg'
+    label, score = classifier.predict_label_with_confidence(image)
+    print(f"label: {label}, score: {score}")
+    assert label == "beagle"
+    assert score > 0.1
+
