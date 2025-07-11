fa4120f739126cfa40cec138baa9e6445c0a112b
Xiaonan Zhao <zhaoxxn@gmail.com>
2025-07-11 18:34:59 +0000

    Add helper function easily run jobs in background and capture its output

--- easydl/utils.py
+++ easydl/utils.py
@@ -4,6 +4,9 @@ Utility functions for deep learning tasks.
 import torch
 import numpy as np
 from easydl.config import *
+import multiprocessing
+import sys
+import traceback
 
 class AcceleratorSetting:
     device = None
@@ -85,3 +88,45 @@ def torch_load_with_prefix_removal(model_param_path):
     if count_of_removed_prefix != len(list(model_param.keys())):
         print(f"Number of removed prefix 'module.' {count_of_removed_prefix} does not match the number of model parameters {len(list(model_param.keys()))}")
     return new_model_param
+
+def run_in_background(target_task, log_file, args=(), kwargs={}):
+    """
+    A wrapper to run a function in a background process, redirecting its
+    stdout and stderr to a log file.
+
+    This is useful for capturing all print statements and errors from the task.
+
+    :param target_task: The function to run in the background.
+    :param log_file: The path to the file where output will be logged.
+    :param args: A tuple of arguments to pass to the target_task.
+    :param kwargs: A dictionary of keyword arguments to pass to the target_task.
+    :return: The multiprocessing.Process object that was started.
+    """
+    
+    def task_wrapper():
+        """
+        This inner function is what actually runs in the new process.
+        It sets up the I/O redirection before calling the user's target function.
+        """
+        print(f"Background process started. Logging to '{log_file}'. PID: {multiprocessing.current_process().pid}")
+        
+        # We open the log file in 'append' mode ('a') so we don't lose logs
+        # if the script is run multiple times.
+        with open(log_file, 'a') as f:
+            # Redirect both standard output and standard error to the log file
+            sys.stdout = f
+            sys.stderr = f
+            
+            try:
+                # Now, execute the original target function with its arguments
+                target_task(*args, **kwargs)
+            except Exception as e:
+                # If the task crashes, log the exception traceback to the file
+                print(f"\n--- An unhandled exception occurred in the background task ---")
+                traceback.print_exc()
+                print("------------------------------------------------------------")
+
+    # Create a new process targeting our wrapper function
+    process = multiprocessing.Process(target=task_wrapper, daemon=True)
+    process.start()
+    return process
